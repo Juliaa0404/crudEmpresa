7dc978c1302d72f58c1adf001e617a06f4c9c352
Julia <juliarufato008@gmail.com>
2025-09-29 20:42:50 -0300

    alteração service

--- src/main/java/com/example/crudSpring/projetoCRUD/CONTROLLER/EmpresaController.java
+++ src/main/java/com/example/crudSpring/projetoCRUD/CONTROLLER/EmpresaController.java
@@ -6,9 +6,13 @@ import org.springframework.ui.Model;
 import com.example.crudSpring.projetoCRUD.ENTITY.Empresa;
 import com.example.crudSpring.projetoCRUD.SERVICE.EmpresaService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -26,10 +30,21 @@ public class EmpresaController {
         this.empresaService = ligacaoEmpresaService;
     }
 
+    
+    
     @GetMapping("/viewCadEmpresa")
     public String mostrarFormCadastro(Model oModel){//model serve para chamar o frontend
         oModel.addAttribute("empresa", new Empresa());
         return "cadastroEmpresa";
     }
+
+    // @ = notation
+    @PostMapping("/salvarEmpresa")
+    public String salvarEmpresa( @ModelAttribute Empresa objEmpresa) {
+       //chamando o método cadastrar e passando o objeto("pacotinho") com os dados que precisam ser salvos
+        empresaService.cadastrarEmpresa(objEmpresa);
+        return "redirect:/empresaCTR/viewCadEmpresa";
+    }
+    
     
 }
