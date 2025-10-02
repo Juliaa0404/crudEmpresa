9737704aa0b6279e22d82a6a0c46f7e72fe83b1b
Júlia <juliarufato008@gmail.com>
2025-10-02 20:43:18 -0300

    (Descrição das alterações)

--- src/main/java/com/example/crudSpring/projetoCRUD/CONTROLLER/EmpresaController.java
+++ src/main/java/com/example/crudSpring/projetoCRUD/CONTROLLER/EmpresaController.java
@@ -5,13 +5,13 @@ import org.springframework.ui.Model;
 
 import com.example.crudSpring.projetoCRUD.ENTITY.Empresa;
 import com.example.crudSpring.projetoCRUD.SERVICE.EmpresaService;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/empresaCTR")
-
 
 public class EmpresaController {
     
@@ -30,6 +29,11 @@ public class EmpresaController {
         this.empresaService = ligacaoEmpresaService;
     }
 
+    @GetMapping("/listarTodasEmpresas")
+    public String listarEmpresa(Model oModel) {
+        oModel.addAttribute("empresas", empresaService.findAll());
+        return "listarEmpresas";
+    }
     
     
     @GetMapping("/viewCadEmpresa")
@@ -43,7 +47,7 @@ public class EmpresaController {
     public String salvarEmpresa( @ModelAttribute Empresa objEmpresa) {
        //chamando o método cadastrar e passando o objeto("pacotinho") com os dados que precisam ser salvos
         empresaService.cadastrarEmpresa(objEmpresa);
-        return "redirect:/empresaCTR/viewCadEmpresa";
+        return "redirect:/empresaCTR/listarTodasEmpresas";
     }
     
     
