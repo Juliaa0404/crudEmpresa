4b0c28b82fd20b1482643d4a2d3b72479890ae5f
Julia <juliarufato008@gmail.com>
2025-10-13 20:39:03 -0300

    alteração service

--- src/main/java/com/example/crudSpring/projetoCRUD/CONTROLLER/MainController.java
+++ src/main/java/com/example/crudSpring/projetoCRUD/CONTROLLER/MainController.java
@@ -2,11 +2,18 @@ package com.example.crudSpring.projetoCRUD.CONTROLLER;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 
 @Controller
 @RequestMapping("/")
 public class MainController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "main";
+    }
     
 }
