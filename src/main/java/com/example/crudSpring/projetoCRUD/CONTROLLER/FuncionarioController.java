c5bb9773738d4bebe04ceaa058f60dc3247a22e9
Julia <juliarufato008@gmail.com>
2025-11-13 20:45:30 -0300

    (13-11)

--- src/main/java/com/example/crudSpring/projetoCRUD/CONTROLLER/FuncionarioController.java
+++ src/main/java/com/example/crudSpring/projetoCRUD/CONTROLLER/FuncionarioController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.example.crudSpring.projetoCRUD.ENTITY.Funcionario;
 import com.example.crudSpring.projetoCRUD.SERVICE.EmpresaService;
 import com.example.crudSpring.projetoCRUD.SERVICE.FuncionarioService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -39,6 +43,15 @@ public class FuncionarioController {
         oModel.addAttribute("funcionario", new Funcionario());
         oModel.addAttribute("empresas", ligacaoEmpresaService.findAll());
         return "cadastrarFuncionario";
+    }
+    
+    @PostMapping("/salvarFuncionario")
+    public String cadastrarFuncionario(@ModelAttribute Funcionario objFuncionario) {
+        
+        ligacaoFuncionarioService.cadastrarFuncionario(objFuncionario);
+        
+        
+        return "redirect:/funcionarioCTR/listarFunc";
     }
     
 }
